8b4f27fd4016b70c28dbd9768ac7344ed521a5b8
Gerardo Manuel Muñoz <gerardo.manuel3@gmail.com>
2021-11-05 22:14:49 -0300

    actualizacion de SQL

--- problemaPractico/inversiones.sql
+++ problemaPractico/inversiones.sql
@@ -1,131 +1,164 @@
-CREATE TABLE `usuarios` (
+/*
+Navicat MySQL Data Transfer
 
-`id` int(11) NOT NULL AUTO_INCREMENT,
+Source Server         : pos sportlife localhost
+Source Server Version : 100501
+Source Host           : localhost:3306
+Source Database       : inversiones
 
-`nombre` varchar(100) NULL,
+Target Server Type    : MYSQL
+Target Server Version : 100501
+File Encoding         : 65001
 
-`email` varchar(255) NOT NULL,
+Date: 2021-11-05 22:13:26
+*/
 
-PRIMARY KEY (`id`, `email`) 
+SET FOREIGN_KEY_CHECKS=0;
 
-);
-
-
-
+-- ----------------------------
+-- Table structure for cartera
+-- ----------------------------
+DROP TABLE IF EXISTS `cartera`;
 CREATE TABLE `cartera` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idUsuario` int(11) NOT NULL,
+  `descripcion` text DEFAULT NULL,
+  `estadoActivo` bit(1) DEFAULT b'0',
+  PRIMARY KEY (`id`),
+  KEY `idUsuario_index` (`idUsuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
-`id` int(11) NOT NULL AUTO_INCREMENT,
+-- ----------------------------
+-- Records of cartera
+-- ----------------------------
+INSERT INTO `cartera` VALUES ('1', '3', 'Careta Risopatron', '');
+INSERT INTO `cartera` VALUES ('2', '3', 'test', '');
+INSERT INTO `cartera` VALUES ('3', '3', 'test', '');
+INSERT INTO `cartera` VALUES ('4', '3', 'test', '');
 
-`idUsuario` int(11) NOT NULL,
+-- ----------------------------
+-- Table structure for estadoorden
+-- ----------------------------
+DROP TABLE IF EXISTS `estadoorden`;
+CREATE TABLE `estadoorden` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
-`descripcion` text NULL,
+-- ----------------------------
+-- Records of estadoorden
+-- ----------------------------
+INSERT INTO `estadoorden` VALUES ('1', 'iniciada');
+INSERT INTO `estadoorden` VALUES ('2', 'espera');
+INSERT INTO `estadoorden` VALUES ('3', 'terminada');
 
-`estadoActivo` bit NULL DEFAULT 0,
+-- ----------------------------
+-- Table structure for instrumentos
+-- ----------------------------
+DROP TABLE IF EXISTS `instrumentos`;
+CREATE TABLE `instrumentos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) DEFAULT NULL,
+  `precioApertura` float DEFAULT NULL,
+  `precioCierre` float DEFAULT NULL,
+  `codigo` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
-PRIMARY KEY (`id`) ,
+-- ----------------------------
+-- Records of instrumentos
+-- ----------------------------
+INSERT INTO `instrumentos` VALUES ('1', 'ENELGXCH', '180', '178', 'CLP3710M1090');
+INSERT INTO `instrumentos` VALUES ('2', 'ENTEL', '2996', '3098', 'CLP371151059');
+INSERT INTO `instrumentos` VALUES ('3', 'GOLF', '16500000', '16500000', 'CLP4852V1053');
+INSERT INTO `instrumentos` VALUES ('4', 'ENJOY', '2.751', '2.746', 'CL0000006586');
+INSERT INTO `instrumentos` VALUES ('5', 'MANQUEHUE', '49.73', '51.75', 'CL0002416189');
+INSERT INTO `instrumentos` VALUES ('6', 'SALFACORP', '297.02', '310', 'CL0000000449');
+INSERT INTO `instrumentos` VALUES ('7', 'FALABELLA', '2425', '2580.1', 'CLP3880F1085');
+INSERT INTO `instrumentos` VALUES ('8', 'MARINSA', '43.74', '43.74', 'CLP6444R1042');
+INSERT INTO `instrumentos` VALUES ('9', 'SONDA', '300.55', '312.93', 'CL0000001934');
+INSERT INTO `instrumentos` VALUES ('10', 'PROVIDA', '685.98', '690', 'CLP7919K1035');
 
-INDEX `idUsuario_index` (`idUsuario` ASC)
+-- ----------------------------
+-- Table structure for ordeninversiones
+-- ----------------------------
+DROP TABLE IF EXISTS `ordeninversiones`;
+CREATE TABLE `ordeninversiones` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idCartera` int(11) NOT NULL,
+  `idTipoOrden` int(11) NOT NULL,
+  `idInstrumento` int(11) NOT NULL,
+  `idEstadoOrden` int(11) NOT NULL,
+  `fechaIntroduccion` datetime DEFAULT NULL ON UPDATE current_timestamp(),
+  `fechaEjecucion` date DEFAULT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  `precioEjecucion` float DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idCartera_index` (`idCartera`),
+  KEY `idTipoOrden_index` (`idTipoOrden`),
+  KEY `idInstrumento_index` (`idInstrumento`),
+  KEY `idEstadoOrden_index` (`idEstadoOrden`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
-);
+-- ----------------------------
+-- Records of ordeninversiones
+-- ----------------------------
+INSERT INTO `ordeninversiones` VALUES ('1', '1', '1', '4', '3', '2021-11-02 22:25:51', '2021-11-02', '1', '214.9');
+INSERT INTO `ordeninversiones` VALUES ('2', '1', '1', '1', '3', '2021-11-03 00:58:56', '2021-11-03', '500', '5');
+INSERT INTO `ordeninversiones` VALUES ('3', '1', '1', '1', '3', '2021-11-03 00:58:33', '2021-11-03', '10', '10');
+INSERT INTO `ordeninversiones` VALUES ('4', '1', '1', '1', '3', '2021-11-03 00:59:00', '2021-11-03', '10', '10');
 
+-- ----------------------------
+-- Table structure for tipoorden
+-- ----------------------------
+DROP TABLE IF EXISTS `tipoorden`;
+CREATE TABLE `tipoorden` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
+-- ----------------------------
+-- Records of tipoorden
+-- ----------------------------
+INSERT INTO `tipoorden` VALUES ('1', 'acciones');
 
-CREATE TABLE `ordenInversiones` (
+-- ----------------------------
+-- Table structure for usuariohistorico
+-- ----------------------------
+DROP TABLE IF EXISTS `usuariohistorico`;
+CREATE TABLE `usuariohistorico` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idUsuario` int(11) NOT NULL,
+  `año` int(11) NOT NULL,
+  `mes` int(11) NOT NULL,
+  `importeInicial` float DEFAULT NULL,
+  `importeFinal` float DEFAULT NULL,
+  `beneficio` float DEFAULT NULL,
+  `porcentaje` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`,`año`,`mes`),
+  KEY `idUsuario_index` (`idUsuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-`id` int(11) NOT NULL AUTO_INCREMENT,
+-- ----------------------------
+-- Records of usuariohistorico
+-- ----------------------------
 
-`idCartera` int(11) NOT NULL,
+-- ----------------------------
+-- Table structure for usuarios
+-- ----------------------------
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`,`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
-`idTipoOrden` int(11) NOT NULL,
-
-`idInstrumento` int(11) NOT NULL,
-
-`idEstadoOrden` int(11) NOT NULL,
-
-`fechaIntroduccion` datetime NULL ON UPDATE CURRENT_TIMESTAMP,
-
-`fechaEjecucion` date NULL,
-
-`cantidad` int NULL,
-
-`precioEjecucion` float NULL,
-
-PRIMARY KEY (`id`) ,
-
-INDEX `idCartera_index` (`idCartera` ASC),
-
-INDEX `idTipoOrden_index` (`idTipoOrden` ASC),
-
-INDEX `idInstrumento_index` (`idInstrumento` ASC),
-
-INDEX `idEstadoOrden_index` (`idEstadoOrden` ASC)
-
-);
-
-
-
-CREATE TABLE `intrumentos` (
-
-`id` int(11) NOT NULL AUTO_INCREMENT,
-
-`nombre` varchar(255) NULL,
-
-`precioApertura` float NULL,
-
-`precioCierre` float NULL,
-
-`codigo` varchar(15) NULL,
-
-PRIMARY KEY (`id`) 
-
-);
-
-
-
-CREATE TABLE `usuarioHistorico` (
-
-`id` int(11) NOT NULL AUTO_INCREMENT,
-
-`idUsuario` int(11) NOT NULL,
-
-`año` int NOT NULL,
-
-`mes` int NOT NULL,
-
-`importeInicial` float NULL,
-
-`importeFinal` float NULL,
-
-`beneficio` float NULL,
-
-`porcentaje` int NULL,
-
-PRIMARY KEY (`id`, `año`, `mes`) ,
-
-INDEX `idUsuario_index` (`idUsuario` ASC)
-
-);
-
-
-
-CREATE TABLE `tipoOrden` (
-
-`id` int(11) NOT NULL AUTO_INCREMENT,
-
-`descripcion` varchar(150) NULL,
-
-PRIMARY KEY (`id`) 
-
-);
-
-
-
-CREATE TABLE `estadoOrden` (
-
-`id` int(11) NOT NULL,
-
-`descripcion` varchar(50) NULL,
-
-PRIMARY KEY (`id`) 
-
-);
+-- ----------------------------
+-- Records of usuarios
+-- ----------------------------
+INSERT INTO `usuarios` VALUES ('1', 'Risopatron', 'gerar1do.m1anuel@gma123il.com');
+INSERT INTO `usuarios` VALUES ('2', 'Risopatron', 'gerardo.manuel@gmail.com');
+INSERT INTO `usuarios` VALUES ('3', 'Risopatron', 'gerardo.manuel3@gmail.com');
